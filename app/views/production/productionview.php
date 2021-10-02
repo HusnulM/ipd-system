1f775c936e3c92fa1e6d89bdc2b65fb96c57d917
HusnulM <husnulmub@gmail.com>
2021-10-02 12:36:30 +0700

    update

--- app/views/production/productionview.php
+++ app/views/production/productionview.php
@@ -11,6 +11,9 @@
                 <div class="row">
                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                         <div class="card">
+                            <div class="header">
+                                <h3>Production Monitoring</h3>
+                            </div>
                             <div class="body">
                                 <div class="row">                   
                                     <div class="col-lg-12">
